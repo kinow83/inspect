c698b586729f6ad981c116aba37162cc8e559b02
kinow <kinow83@gmail.com>
2020-08-20 00:20:17 +0900

    sqlite3 install

--- src/modules/module_sqlite3.c
+++ src/modules/module_sqlite3.c
@@ -2,7 +2,8 @@
 #include "module_sqlite3.h"
 
 // https://mangsby.com/blog/programming/c/c-c-sqlite-%EB%8D%B0%EC%9D%B4%ED%84%B0%EB%B2%A0%EC%9D%B4%EC%8A%A4-%EC%82%AC%EC%9A%A9%ED%95%98%EA%B8%B0-1-%EA%B8%B0%EB%B3%B8-%EB%B2%88%EC%97%AD/
-
+// apt-get install sqlite3
+// apt-get install sqlitebrowser
 
 static void* module_init_sqlite3() {
     module_sqlite3_ctx_t *ctx = NULL;
